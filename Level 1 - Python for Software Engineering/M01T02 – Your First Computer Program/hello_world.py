e635e0ccf7d27abfaa4fc63f6f6517ad99903804
Andrew <andrewdriessen1@gmail.com>
2026-01-06 23:49:39 +1100

    Create hello_world.py for user input
    Added a simple program to get user input for name and age.

---
+++ Level 1 - Python for Software Engineering/M01T02 – Your First Computer Program/hello_world.py
@@ -0,0 +1,10 @@
+# get users name
+# print users name
+# get users age
+# print users age
+
+user_name = input("Ënter your name")
+print(user_name)
+user_age = input("Ënter your age")
+print(user_age)
+print("Hello, World!")
